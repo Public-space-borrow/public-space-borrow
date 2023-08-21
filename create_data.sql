c6b2dcda74a829282c99d71d66467af98b9f2fe8
chicheng567 <samuel910617@yahoo.com.tw>
2023-08-21 08:11:49 +0800

    database_create

---
+++ create_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Space (Space_name, region) VALUES ("雨樹L棟會議室", "雨樹");
+INSERT INTO Space (Space_name, region) VALUES ("雨樹廣場", "雨樹");
+INSERT INTO Space (Space_name, region) VALUES ("雨樹L棟自煮空間", "雨樹");
+INSERT INTO Space (Space_name, region) VALUES ("雨樹藝文空間", "雨樹");
