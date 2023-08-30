ed1d7f86fc916e24089c9bbec8df399d0cba226f
邱 <studyinglu82@gmail.com>
2023-08-31 03:01:06 +0800

    ya
    ya

--- apply.php
+++ apply.php
@@ -27,14 +27,22 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>宿舍公共空間借用系統</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/styles.css" rel="stylesheet" />
         <link href="css/apply.css" rel="stylesheet" />
-        
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+        <!-- Bootstrap core JS-->
+        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script> -->
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <script src="js/apply.js"></script>
+        <script src="js/change_select.js"></script>
+        <script src="plugin/jspanel-4.16.1/dist/jspanel.min.js"></script>
+        <link href="plugin/jspanel-4.16.1/dist/jspanel.min.css" rel="stylesheet">
+        <script src="plugin/jspanel-4.16.1/dist/extensions/modal/jspanel.modal.min.js"></script>
     </head>
     <body class="d-flex flex-column">
         <main class="flex-shrink-0">
