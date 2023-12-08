49888f85beb58b3be1b35d40c40ff4618106b6aa
chicheng567 <samuel910617@yahoo.com.tw>
2023-12-08 18:40:51 +0800

    online v1.0, replace connect db proccess with including connect_db.php

--- add_register.php
+++ add_register.php
@@ -1,8 +1,29 @@
 <?php
+function check_sequence($conn, $space_id, $user_id, $date, $start_time) {
+    $stmt = $conn->prepare(sprintf("select Start_time from Register where user_id = '%s' and Date = '%s' and Space_id = %d ORDER BY Start_time", $user_id, $date, $space_id));
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    $arr = array($start_time);
+    foreach($result as $row) {
+        array_push($arr, $row['Start_time']);
+    }
+    sort($arr);
+    $count = 1;
+    for($i = 1; $i < sizeof($arr); $i++) {
+        if($arr[$i] - $arr[$i - 1] == 1) {
+            $count++;
+        }
+        else {
+            if($count > 2) {
+                return 1;
+            }
+            $count = 1;
+        }
+    }
+    return $count > 2;
+}
 if(isset($_POST['mode'])) {
-    $servername = "114.35.222.181";
-    $username = "dormcenter";
-    $password = "59365937";
+    include 'connect_db.php';
     $day = [
         "一" => 1,
         "二" => 2,
@@ -12,26 +33,35 @@ if(isset($_POST['mode'])) {
         "六" => 6,
         "日" => 7,
     ];
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=dorm", $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      } catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
     if($_POST['mode'] == "add") {
+        //check black list
+        $b_state = $conn->prepare(sprintf("select * from black_list where stu_id = '%s'", $_POST['user_id']));
+        $b_state->execute();
+        $black_list = $b_state->fetch();
+        if($black_list != NULL) {
+            echo "此學號已被列入黑名單"."\n原因:".$black_list['banned_reason']."\n解除日期".$black_list['expire_time']."\n若有疑問請至宿舍服務組詢問";
+            return;
+        }
+        //insert
         $sql = sprintf("INSERT INTO Register (Start_time, Space_id, Date, user_id, user_phone, user_dormnumber, change_pwd, user_name) VALUES(%d, %s, '%s', '%s', '%s', %d, '%s', '%s')", $_POST['Start_time'], $_POST['Space_id'], $_POST['date'], $_POST['user_id'], $_POST['user_phone'], $_POST['user_dormnumber'], $_POST['change_pwd'], $_POST['name']);
         $stmt = $conn->prepare(sprintf("select COUNT(*) as c from Register where Start_time = %d and Date = '%s' and Space_id = %d", $_POST['Start_time'], $_POST['date'], $_POST['Space_id']));
         $stmt->execute();
         $row = $stmt->fetch();
+
+        $stmt = $conn->prepare(sprintf("select COUNT(*) as c from Register where user_id = %d and Date = '%s' and Space_id = %d", $_POST['user_id'], $_POST['date'], $_POST['Space_id']));
+        $stmt->execute();
+        $count_have_regist = $stmt->fetch();
         if($row['c'] > 0) {
             echo "這個時段已經被預約！";
+        }
+        else if($count_have_regist['c'] >= 2 && check_sequence($conn, $_POST['Space_id'], $_POST['user_id'], $_POST['date'], (int)$_POST['Start_time'])) {
+            echo "此空間不可連續預約2小時以上";
         }
         else {
             try {
                 $stmt = $conn->prepare($sql);
                 $stmt->execute();
-                echo "預約成功";
+                echo "預約成功"."\n使用前請至服務站出示學生證以借用鑰匙";
             }
             catch(Exception $e) {
                 echo $e;
@@ -65,7 +95,8 @@ if(isset($_POST['mode'])) {
         $stmt->execute();
         $row = $stmt->fetch();
         if($row['change_pwd'] == $_POST['change_pwd']) {
-            $conn->query($sql);
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
             echo "刪除成功！";
         }
         else {
--- request_regist.php
+++ request_regist.php
@@ -1,18 +1,20 @@
 <?php
+    session_start();
     if(isset($_POST['id'])) {
-        $servername = "114.35.222.181";
-        $username = "dormcenter";
-        $password = "59365937";
-        try {
-            $conn = new PDO("mysql:host=$servername;dbname=dorm", $username, $password);
-            // set the PDO error mode to exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-        }
+        include 'connect_db.php';
         // echo "SELECT * FROM Register where Space_id = ".$_POST['id']." ORDER BY Start_time;";
-        $stmt = $conn->query("SELECT * FROM Register where Space_id = ".$_POST['id']." ORDER BY Start_time;");
+        $stmt = $conn->query("SELECT Start_time, Space_id, Date, user_id, user_name, user_phone, user_dormnumber FROM Register where Space_id = ".$_POST['id']." ORDER BY Start_time;");
         $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if($_SESSION['mode'] != "admin") {
+            for($i = 0; $i < count($data); $i++) {
+                $ori_name = $data[$i]['user_name'];
+                $encode_name = mb_substr($ori_name, 0, 1)."Ｏ".mb_substr($ori_name, 2, 1);
+                $data[$i]['user_name'] = $encode_name;
+                $data[$i]['user_id'] = mb_substr($data[$i]['user_id'], 0, 5)."ＯＯＯＯＯ";
+                #如果非管理者，直接不要傳手機號碼
+                $data[$i]['user_phone'] = "";
+            }
+        }
         $json = json_encode($data);
         // 使用echo函數將字串輸出給前端
         echo $json;
