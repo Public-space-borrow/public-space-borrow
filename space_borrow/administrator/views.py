87b2b0224ffd7308ac6b771c1556abdae7118ce0
chicheng <samuel910617@yahoo.com.tw>
2024-03-29 10:47:57 +0800

    get_emanil form

--- space_borrow/administrator/views.py
+++ space_borrow/administrator/views.py
@@ -70,7 +70,14 @@ def BlackList_edit(request):
 
 def stu_info(request):
     if request.session['identity'] == "private":
-        
-        return render(request, "get_info.html", {"form":StudentID})
+        if request.method == "GET":
+            return render(request, "get_info.html")
+        elif request.method == "POST":
+            form = StudentID(request.POST)
+            if form.is_valid():
+                ids = form.cleaned_data['ids']
+            
+            return HttpResponse(ids)
     else:
         raise Http404("Page not exit")
+    
